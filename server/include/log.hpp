f32e95a08cd5dfbd759bca0e8e00fd3bda2a4db2
Cody Nault <naultcr@gmail.com>
2015-07-28 09:32:10 -0700

    Revert "updated logging"
    This reverts commit ee88f4ad943139be1627d8fa08071bc28230f2fd.

--- server/include/log.hpp
+++ server/include/log.hpp
@@ -2,24 +2,16 @@
 #define SUEDE_LOG_H_
 
 #include <stdio.h>
-#include <ctime>
-#include <string>
 
 class Log
 {
 	static FILE * LogFile;
 	static bool LogToConsole;
-	static time_t t;
-	static struct tm * SysTime;
-	static char FormattedTime[80];
-	
-	static const char LogFileName[128]; // This will be replaced with a config file variable eventually
 	
 	public:
-	static char * TimeStamp(void);
 	static void Initialize(void);
 	static void Initialize(int);
-	static void LogEvent(int, const char *);
+	static void LogEvent(const char *);
 	static void Finalize(void);
 };
 
