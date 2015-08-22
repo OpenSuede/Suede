ab90d3a98cf587be4e36d7259e4c14af8618c252
Garrett Morrison <garrett.d.morrison@gmail.com>
2015-08-22 12:26:24 -0700

    Updated Log for TrafficLogging

--- server/include/log.hpp
+++ server/include/log.hpp
@@ -7,13 +7,16 @@
 
 class Log
 {
-	static FILE * LogFile;
+	static FILE * SysLogFile;
+	static FILE * TrafficLogFile;
 	static bool LogToConsole;
+	static bool LogTraffic;
 	static time_t t;
 	static struct tm * SysTime;
 	static char FormattedTime[80];
 	
-	static const char LogFileName[128]; // This will be replaced with a config file variable eventually
+	static const char S_LogFileName[128]; // This will be replaced with a config file variable eventually
+	static const char T_LogFileName[128]; // This will be replaced with a config file variable eventually
 	static std::mutex AccessLog;
 	static std::mutex Logging;
 	static std::mutex Clock;
