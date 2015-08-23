549d9eeac82557eff629272323c94464342d0bf4
Cody Nault <naultcr@gmail.com>
2015-08-22 17:14:05 -0700

    created new config class

---
+++ server/include/config.hpp
@@ -0,0 +1,13 @@
+#ifndef CONFIG_LOG_H
+#define CONFIG_LOG_H
+
+#include <stdio.h>
+#include <string>
+
+class Config
+{	
+	public:
+	static bool Initialize(void);
+};
+
+#endif
---
+++ server/test/config_test.cpp
@@ -0,0 +1,2 @@
+#include "config.hpp"
+#include "gtest/gtest.h"
